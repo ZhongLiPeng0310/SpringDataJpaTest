394615e837409a93c27b84d87a9e28425e694059
zhonglipeng <zhonglipeng@lbx.com>
2020-12-21 14:58:35 +0800

    111

--- many-to-many/src/main/java/com/manong/entity/Project.java
+++ many-to-many/src/main/java/com/manong/entity/Project.java
@@ -26,6 +26,12 @@ public class Project {
      * 1.声明表关系的配置
      * @ManyToMany(targetEntity = Employee.class) //多对多
      * targetEntity：代表对方的实体类字节码
+     */
+    //一对多
+    @ManyToMany(targetEntity =Employee.class ,cascade = CascadeType.ALL)
+    private Set<Employee> employees = new HashSet<>();
+
+    /*
      * 2.配置中间表（包含两个外键）
      * @JoinTable
      * name : 中间表的名称
@@ -35,10 +41,6 @@ public class Project {
      * referencedColumnName：参照的主表的主键名
      * inverseJoinColumns：配置对方对象在中间表的外键
      */
-    //一对多
-    @ManyToMany(targetEntity =Employee.class ,cascade = CascadeType.ALL)
-    private Set<Employee> employees = new HashSet<>();
-
     //第三张表（外键关系表、中间表）
     //name属性：第三张表的表名称
     @JoinTable(name = "t_employee_project",
